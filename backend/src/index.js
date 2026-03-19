d674037169a31930c3a21aca205d9dc2ac1c98e6
serjaydc <serhiinote@gmail.com>
2026-03-19 17:08:56 +0000

    Backend & Frontend Setup

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+});
